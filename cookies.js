19e0bf4fdf7dc649b39c03a64b3d996bbf1a66ec
Guntis11 <92687187+Guntis11@users.noreply.github.com>
2021-11-01 01:04:18 +0300

    Add files via upload

---
+++ cookies.js
@@ -0,0 +1,61 @@
+"use strict"
+
+let Nomaina = 0;
+
+function Nospied() {
+    let krasa = document.getElementById("body");
+    switch(Nomaina){
+      case 0: 
+        krasa.style.backgroundColor = "grey";
+        Nomaina++;
+        document.cookie = "Krasa=grey";
+
+        break;
+      case 1:
+        krasa.style.backgroundColor = "azure";
+        Nomaina--;
+        document.cookie = "Krasa=azure";
+
+        break;  
+    }
+}
+
+setCookie();
+function setCookie(){
+  let cookie = document.cookie;
+  if(cookie.getCookie != "Krasa") {
+    document.cookie = "Krasa=azure";
+
+  }
+}
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+readCookie();
+function readCookie(){
+  let krasa = getCookie("Krasa");
+  switch(krasa){
+    case "grey":
+      krasa.style.backgroundColor = "grey";
+      Nomaina = 1;
+      break;
+
+    case "azure":
+      krasa.style.backgroundColor = "azure";
+      Nomaina = 0;
+      break;
+  }    
+}
